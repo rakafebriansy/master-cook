b3cd67d7582783ae2549f5ebfec5c37a3c863605
Yusrina55 <yusrinaaa05@gmail.com>
2024-06-03 15:40:31 +0700

    add dashboard, akun, verify akun

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".views/**/*.{html,js}"],
+  content: ["./views/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light:{
+          logo: '#15355E',   
+          button : '#A43131', 
+          klik : '#7E1E1E',
+          header : '#F3F4F6', 
+        }
+      },
+      fontFamily :{
+        roboto : ['Roboto'],
+      },
+    },
   },
   plugins: [],
 }
